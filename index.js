345ddf4118a78d65f2c96ba9424bb39b3d76e7db
Imelda Hope <imeldalarusso@gmail.com>
2024-07-01 14:19:49 +0300

    creates .find() on array

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(arr){
+    const win = arr.find(arr => arr.result === "W");
+
+    if(win) {        
+        return win.year;
+    } 
+}
+
+superbowlWin()
